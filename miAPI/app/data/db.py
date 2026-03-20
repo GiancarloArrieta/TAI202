20347bc0a139f66801113ba9ef7c4f9a43295d8a
GiancarloArrieta <123045650@upq.edu.mx>
2026-03-19 20:29:52 -0600

    SQLAlchemy Parte 1

---
+++ miAPI/app/data/db.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+
+# 1. Definir la URL de conexión
+DATABASE_URL = os.getenv(
+	"DATABASE_URL",
+	"postgresql://admin:123456@postgres:5433/DB_miapi"
+)
+
+# 2. Creación de motor de conexión
+engine = create_engine(DATABASE_URL)
+
+# 3. Preparación de gestionador de sesiones
+SesionLocal = sessionmaker(
+	autocommit = False,
+	autoflush = False,
+	bind = engine
+)
+
+# 4. Base declarativa para los modelos
+Base = declarative_base()
+
+# 5. Obtener sesiones de cada petición
+def get_db():
+	db = SesionLocal()
+	try:
+		yield db
+	finally:
+		db.close()	
